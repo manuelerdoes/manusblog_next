5420e70b49e5e5859f3dfa9954e41ade6cedb558
Manuel Erdös <manuel.erdoes@bluewin.ch>
2024-10-13 23:24:49 +0500

    moved sqlPassword to .env

--- app/lib/const.js
+++ app/lib/const.js
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const pageTitle = "Manus Blog";
 export const adminUserId = "2XKfBCU9BYWK05cYCRdzVxAzRfZ2";
 export const sqlServer = "localhost";
 export const sqlUser = "manusblog";
-export const sqlPassword = "abcd1234";
+export const sqlPassword = process.env.SQL_PASSWORD;
 export const sqlDatabase = "manusblog";
