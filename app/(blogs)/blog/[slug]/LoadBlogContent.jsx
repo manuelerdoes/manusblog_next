712a179f51d7d871303711b77569ff4a6517c7b7
Manuel Erdös <manuel.erdoes@bluewin.ch>
2024-12-11 21:38:20 +0700

    content formatting

--- app/(blogs)/blog/[slug]/LoadBlogContent.jsx
+++ app/(blogs)/blog/[slug]/LoadBlogContent.jsx
@@ -1,7 +1,32 @@
 import { apiServer } from '@/app/lib/const';
-import React from 'react'
 import DOMPurify from "isomorphic-dompurify";
 import { marked } from 'marked';
+import prism from "prismjs";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-json";
+import "prismjs/themes/prism-tomorrow.css";
+// import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from 'highlight.js';
+
+// Set up prism and marked options outside the component to prevent unnecessary reconfigurations
+marked.setOptions({
+  highlight: (code, lang) => {
+    const language = prism.languages[lang] || prism.languages.markup;
+    return prism.highlight(code, language, lang);
+  },
+});
+
+// const marked = new Marked(
+//   markedHighlight({
+// 	emptyLangClass: 'hljs',
+//     langPrefix: 'hljs language-',
+//     highlight(code, lang, info) {
+//       const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+//       return hljs.highlight(code, { language }).value;
+//     }
+//   })
+// );
 
 async function getBlog(id) {
   const res = await fetch(`${apiServer}/api/blog/${id}`, {
@@ -13,18 +38,21 @@ async function getBlog(id) {
     console.error("Could not fetch blog content");
     return null;
   }
-  const data = await res.json(); // Ensure the response is parsed as JSON
+  const data = await res.json();
   return data;
 }
 
 async function LoadBlogContent({ blogId }) {
-
   const currentBlog = await getBlog(blogId);
 
-  if (!currentBlog) { 
+  if (!currentBlog) {
     return <div className="loadingerror"><h2>Error</h2><p>Could not load blog content.<br></br>
       You might be trying to access a non-public blogpost. Please check if you are signed in.</p></div>
   }
+
+  const blogContentHTML = DOMPurify.sanitize(marked.parse(currentBlog.content), {
+    ALLOWED_ATTR: ['class'], // Ensure class attributes are not stripped by DOMPurify
+  });
 
   return (
     <>
@@ -33,21 +61,10 @@ async function LoadBlogContent({ blogId }) {
         {!currentBlog.isPublic && (<span>not public</span>)}
       </div>
       <div className="blogcontent">
-        <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked.parse(currentBlog.content)) }} />
-        {/* <div dangerouslySetInnerHTML={{ __html: marked.parse(currentBlog.content) }} /> */}
-        {/* <p>{currentBlog.content}</p> */}
+        <div dangerouslySetInnerHTML={{ __html: blogContentHTML }} />
       </div>
-      {/* {(!currentBlog.disableComments) ? (
-        <Suspense fallback={<p>loading comments...</p>}>
-          <LoadComments blogId={blogId} />
-        </Suspense>
-      ) : (
-        <div className="comments">
-          <p>comments disabled</p>
-        </div>
-      )} */}
     </>
   )
 }
 
-export default LoadBlogContent
+export default LoadBlogContent;
--- app/version.js
+++ app/version.js
@@ -1,6 +1,7 @@
 
-// 1.0.3  13.10.2024    M. Erdös    Added mysql db script and some dbActions
-// 1.0.2  12.10.2024    M. Erdös    Added most of the design elements
-// 1.0.1   1.10.2024    M. Erdös    Initial
+// 1.0.0  11.12.2024    M. Erdös    First state where it can be used productively
+// 0.0.3  13.10.2024    M. Erdös    Added mysql db script and some dbActions
+// 0.0.2  12.10.2024    M. Erdös    Added most of the design elements
+// 0.0.1   1.10.2024    M. Erdös    Initial
 
-export const appVersion = "1.0.3";
+export const appVersion = "1.0.0";
