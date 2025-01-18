c71a5a36128ec53c1085f17c57d267e3edcf7c4c
Manuel Erdös <manuel.erdoes@bluewin.ch>
2025-01-18 20:34:17 +0700

    version

--- app/version.js
+++ app/version.js
@@ -1,7 +1,8 @@
 
+// 1.0.1  18.01.2024    M. Erdös    Bugfix for Blog not showing pictures when unauthenticated
 // 1.0.0  11.12.2024    M. Erdös    First state where it can be used productively
 // 0.0.3  13.10.2024    M. Erdös    Added mysql db script and some dbActions
 // 0.0.2  12.10.2024    M. Erdös    Added most of the design elements
 // 0.0.1   1.10.2024    M. Erdös    Initial
 
-export const appVersion = "1.0.0";
+export const appVersion = "1.0.1";
