a1e979d8ec81583c7286261d17147ec04e8e0db3
Manuel Erdös <manuel.erdoes@bluewin.ch>
2025-02-03 11:54:20 +0700

    version

--- app/version.js
+++ app/version.js
@@ -28,4 +28,4 @@
 // 0.0.2  12.10.2024    M. Erdös    Added most of the design elements
 // 0.0.1   1.10.2024    M. Erdös    Initial
 
-export const appVersion = "1.2.2";
+export const appVersion = "1.2.3";
